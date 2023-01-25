86380d7958dbbb451f6e9d1a1dd501f1d97a99bc
IasminFT <iasmin.torres04@gmail.com>
2023-01-25 13:54:09 -0300

    bootcamp

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace Tarefas.Web.Controllers;
 
 public class TarefasController : Controller
@@ -7,5 +5,15 @@ public class TarefasController : Controller
     public IActionResult Create()
     {
         return View();
+    }
+
+    public IActionResult Index()
+    {
+        var listaDeTarefas = new List<TarefaViewModel>(){
+            new TarefaViewModel () {Titulo = "Escovar os dentes", Descricao = "Usar Sensodine de manhã e não a noite"},
+            new TarefaViewModel () {Titulo = "Arrumar a cama"},
+            new TarefaViewModel () {Titulo = "Por o lixo para fora", Descricao = "somente às terças"},
+        };
+        return View(listaDeTarefas);
     }
 }
